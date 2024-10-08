9463dada0a4bd7922c0378a1d73e978355d4376c
Daniel <danielgh993@gmail.com>
2024-10-07 21:03:33 -0500

    Se agrega el proyecto.

---
+++ curso_de_phyton_primera_seccion.py
@@ -0,0 +1,53 @@
+#________________________________________________________________________________________
+# Esto es un comentario de una linea
+#________________________________________________________________________________________
+"""
+Esto es un comentario
+de varias lineas con
+comillas dobles 
+"""
+#________________________________________________________________________________________
+'''
+Esto es un comentario
+de varias lineas
+con comillas simples.
+'''
+#________________________________________________________________________________________
+
+# Variable 
+my_variable = "Mi variable."
+my_variable = "Mi variable puede cambiar su valor."
+
+#________________________________________________________________________________________
+# Representacion de una constante por convencion. No existe como tal por que puede variar
+MY_CONSTANT = "Mi constante."
+
+#________________________________________________________________________________________
+# Creacion de variables con datos primitivos 
+my_int = 1
+my_float = 1.5
+my_bool = True 
+my_bool = False 
+my_string = "Cadena de texto."
+my_other_string = 'Cadena de texto con comillas simple.'
+
+#________________________________________________________________________________________
+# Imprimir en consola
+print("Hola")
+
+#________________________________________________________________________________________
+# Como ver el tipo de dato de una variable 
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+
+
+
+
+
+
+
+
+
+
---
+++ curso_de_python_segunda_seccion.py
@@ -0,0 +1,45 @@
+#________________________________________________________________________________________
+# operadores aritmeticos
+suma = 10 + 3
+print("Suma: 10 + 3 =")
+print(suma)
+resta = 10 - 3
+print("Resta: 10 - 3 =")
+print(resta)
+multiplicacion = 10 * 3
+print("Multiplicacion: 10 * 3 =")
+print(multiplicacion)
+divison = 10 / 3
+print("Divison: 10 / 3 =")
+print(divison)
+modulo = 10 % 3
+print("Mood: 10 % 3 =")
+print(modulo)
+exponente = 10 ** 3
+print("Exponente: 10 ** 3 =")
+print(exponente)
+division_entera = 10 // 3
+print("Division entera: 10 // 3 =")
+print(division_entera)
+print("\n")
+#________________________________________________________________________________________
+# Interpolacion en cadena de texto operadores aritmeticos
+print(f"Suma: 10 + 3 = {10 + 3}")  
+print(f"Resta: 10 - 3 = {10 - 3}")
+print(f"Multiplicacion: 10 * 3 = {10 * 3}")
+print(f"Division: 10 / 3 = {10 / 3}")
+print(f"Modulo: 10 % 3 = {10 % 3}")
+print(f"Exponente: 10 ** 3 = {10 ** 3}")
+print(f"Division entera: 10 // 3 = {10 // 3}\n")
+
+#________________________________________________________________________________________
+# Interpolacion operadores de comparacion y con variables
+print(f"Igualdad: 10 == 3 {10 == 3}")
+igualdad = 10 == 3
+print("\n")
+print("Igualdad: 10 == 3")
+print(igualdad)
+ 
+
+
+
